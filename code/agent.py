f4f65f66cdaff3e89409a4d66c5efb7f554ecc07
Elepert <emily.lepert@students.olin.edu>
2017-11-27 20:21:42 -0500

    pushing updated working code of taxation

--- code/agent.py
+++ code/agent.py
@@ -2,68 +2,84 @@ import numpy as np
 
 class Agent:
 
-    def __init__(self, loc, taxation, params):
-        """Creates a new agent at the given location.
+	def __init__(self, loc, taxation, params):
+		"""Creates a new agent at the given location.
 
-        loc: tuple coordinates
-        params: dictionary of parameters
-        """
-        self.loc = tuple(loc)
-        self.age = 0
-        self.taxation = taxation
+		loc: tuple coordinates
+		params: dictionary of parameters
+		"""
+		self.loc = tuple(loc)
+		self.age = 0
+		self.taxation = taxation
 
-        # extract the parameters
-        max_vision = params.get('max_vision', 6)
-        max_metabolism = params.get('max_metabolism', 4)
-        min_lifespan = params.get('min_lifespan', 99998)
-        max_lifespan = params.get('max_lifespan', 100000)
-        min_sugar = params.get('min_sugar', 5)
-        max_sugar = params.get('max_sugar', 25)
+		# extract the parameters
+		max_vision = params.get('max_vision', 6)
+		max_metabolism = params.get('max_metabolism', 4)
+		min_lifespan = params.get('min_lifespan', 99998)
+		max_lifespan = params.get('max_lifespan', 100000)
+		min_sugar = params.get('min_sugar', 5)
+		max_sugar = params.get('max_sugar', 25)
 
-        # choose attributes
-        self.vision = np.random.random_integers(max_vision)
-        self.metabolism = np.random.uniform(1, max_metabolism)
-        self.lifespan = np.random.uniform(min_lifespan, max_lifespan)
-        self.sugar = np.random.uniform(min_sugar, max_sugar)
+		# choose attributes
+		self.vision = np.random.random_integers(1,max_vision)
+		self.metabolism = np.random.uniform(1, max_metabolism)
+		self.lifespan = np.random.uniform(min_lifespan, max_lifespan)
+		self.sugar = np.random.uniform(min_sugar, max_sugar)
 
-    def step(self, env):
-        """Look around, move, and harvest.
+	def step(self, env):
+		"""Look around, move, and harvest.
 
-        env: Sugarscape
-        """
-        self.calculate_tax()
-        self.redistribute_wealth(env.get_welfare())
-        self.loc = env.look_around(self.loc, self.vision)
-        if self.taxation:
-            self.sugar = self.sugar + env.harvest(self.loc) - self.metabolism - self.tax + self.welfare
-        else:
-            self.sugar = self.sugar + env.harvest(self.loc) - self.metabolism
-        self.age += 1
+		env: Sugarscape
+		"""
+		
+		self.redistribute_wealth(env.get_welfare())
+		
+		if self.taxation:
+			self.sugar = self.sugar + env.harvest(self.loc) - self.metabolism - self.tax + self.welfare
+		else:
+			self.sugar = self.sugar + env.harvest(self.loc) - self.metabolism
+		self.age += 1
 
-    def is_starving(self):
-        """Checks if sugar has gone negative."""
-        return self.sugar < 0
+	def step_move_taxed(self,env):
+		self.calculate_tax()
+		self.loc = env.look_around(self.loc, self.vision)
 
-    def is_old(self):
-        """Checks if lifespan is exceeded."""
-        return self.age > self.lifespan
+	def is_starving(self):
+		"""Checks if sugar has gone negative."""
+		return self.sugar < self.metabolism
 
-    def get_wealth(self):
-        return self.sugar
+	def is_old(self):
+		"""Checks if lifespan is exceeded."""
+		return self.age > self.lifespan
 
-    def redistribute_wealth(self, welfare):
-        self.welfare = welfare
+	def get_wealth(self):
+		return self.sugar
 
-    def calculate_tax(self):
-        if self.sugar <= 5:
-            self.tax = self.sugar * .3
-        elif self.sugar > 5 and self.sugar <= 12:
-            self.tax = 1.5 + ((self.sugar-5)*.4)
-        elif self.sugar >12 and self.sugar <= 20:
-            self.tax = 4.8 + ((self.sugar-12)*.5)
-        else:
-            self.tax = 10 + ((self.sugar-20)*.6)
+	def redistribute_wealth(self, welfare):
+		self.welfare = welfare
 
-    def get_tax(self):
-        self.calculate_tax()
-        return self.tax
+	def calculate_tax(self):
+		
+		if self.sugar < 5:
+			self.tax = self.sugar * .3
+		elif self.sugar >= 5 and self.sugar < 12:
+			self.tax = 1.5 + ((self.sugar-5)*.4)
+		elif self.sugar >=12 and self.sugar < 20:
+			self.tax = 4.8 + ((self.sugar-12)*.5)
+		else:
+			self.tax = 10 + ((self.sugar-20)*.6)
+
+		"""
+		if self.sugar < 5:
+			self.tax = self.sugar * .3
+		elif self.sugar >= 5 and self.sugar < 12:
+			self.tax = 1.5 + ((self.sugar*.4-5))
+		elif self.sugar >=12 and self.sugar < 20:
+			self.tax = 4.8 + ((self.sugar*.5-12))
+		else:
+			self.tax = 10 + ((self.sugar*.6-20))
+		"""
+
+	def get_tax(self):
+		self.calculate_tax()
+		return self.tax
--- code/sugar_scape.py
+++ code/sugar_scape.py
@@ -7,218 +7,237 @@ from agent import Agent
 
 
 def make_locs(n, m):
-    """Makes array where each row is an index in an `n` by `m` grid.
+	"""Makes array where each row is an index in an `n` by `m` grid.
 
-    n: int number of rows
-    m: int number of cols
+	n: int number of rows
+	m: int number of cols
 
-    returns: NumPy array
-    """
-    left = np.repeat(np.arange(m), n)
-    right = np.tile(np.arange(n), m)
-    return np.transpose([left, right])
+	returns: NumPy array
+	"""
+	left = np.repeat(np.arange(m), n)
+	right = np.tile(np.arange(n), m)
+	return np.transpose([left, right])
 
 def make_visible_locs(vision):
-    """Computes the kernel of visible cells.
-    vision: int distance
-    """
-    def make_array(d):
-        """Generates visible cells with increasing distance."""
-        a = np.array([[-d, 0], [d, 0], [0, -d], [0, d]])
-        np.random.shuffle(a)
-        return a
+	"""Computes the kernel of visible cells.
+	vision: int distance
+	"""
+	def make_array(d):
+		"""Generates visible cells with increasing distance."""
+		a = np.array([[-d, 0], [d, 0], [0, -d], [0, d]])
+		np.random.shuffle(a)
+		return a
 
-    arrays = [make_array(d) for d in range(1, vision+1)]
-    return np.vstack(arrays)
+	arrays = [make_array(d) for d in range(1, vision+1)]
+	return np.vstack(arrays)
 
 class Sugarscape(Cell2D):
-    """Represents an Epstein-Axtell Sugarscape."""
+	"""Represents an Epstein-Axtell Sugarscape."""
 
-    def __init__(self, n, taxation, **params):
-        """Initializes the attributes.
+	def __init__(self, n, taxation, **params):
+		"""Initializes the attributes.
 
-        n: number of rows and columns
-        params: dictionary of parameters
-        """
-        self.n = n
-        self.params = params
-        self.total_welfare = 0
-        self.taxation = taxation
+		n: number of rows and columns
+		params: dictionary of parameters
+		"""
+		self.n = n
+		self.params = params
+		self.total_welfare = 0
+		self.taxation = taxation
 
-        # track variables
-        self.agent_count_seq = []
-        self.agent_metabolism_seq = []
+		# track variables
+		self.agent_count_seq = []
+		self.agent_metabolism_seq = []
 
-        # make the capacity array
-        self.capacity = self.make_capacity()
+		# make the capacity array
+		self.capacity = self.make_capacity()
+		self.occupied = set()
+		# initially all cells are at capacity
+		self.array = self.capacity.copy()
+		self.agents = []
+		# make the agents
+		self.make_agents()
 
-        # initially all cells are at capacity
-        self.array = self.capacity.copy()
 
-        # make the agents
-        self.make_agents()
+	def make_capacity(self):
+		"""Makes the capacity array."""
 
-    def make_capacity(self):
-        """Makes the capacity array."""
+		# compute the distance of each cell from the peaks.
+		x = np.arange(self.n)
+		rows, cols = np.meshgrid(x, x, indexing='ij')
 
-        # compute the distance of each cell from the peaks.
-        x = np.arange(self.n)
-        rows, cols = np.meshgrid(x, x, indexing='ij')
+		# each cell in `rows` contains its own `i` coordinate
+		# each cell in `cols` contains its `j` coordinate
+		dist1 = np.hypot(rows-15, cols-15)
+		dist2 = np.hypot(rows-35, cols-35)
 
-        # each cell in `rows` contains its own `i` coordinate
-        # each cell in `cols` contains its `j` coordinate
-        dist1 = np.hypot(rows-15, cols-15)
-        dist2 = np.hypot(rows-35, cols-35)
+		# each cell in `dist` contains its distance to the closer peak
+		dist = np.minimum(dist1, dist2)
 
-        # each cell in `dist` contains its distance to the closer peak
-        dist = np.minimum(dist1, dist2)
+		# cells in the capacity array are set according to dist from peak
+		a = np.zeros((self.n, self.n), np.float)
+		a[dist<21] = 1
+		a[dist<16] = 2
+		a[dist<11] = 3
+		a[dist<6] = 4
 
-        # cells in the capacity array are set according to dist from peak
-        a = np.zeros((self.n, self.n), np.float)
-        a[dist<21] = 1
-        a[dist<16] = 2
-        a[dist<11] = 3
-        a[dist<6] = 4
+		return a
 
-        return a
+	def make_agents(self):
+		"""Makes the agents."""
 
-    def make_agents(self):
-        """Makes the agents."""
+		# determine where the agents start and generate locations
+		#n, m = self.params.get('starting_box', self.array.shape)
+		#locs = make_locs(n, m)
+		#np.random.shuffle(locs)
 
-        # determine where the agents start and generate locations
-        n, m = self.params.get('starting_box', self.array.shape)
-        locs = make_locs(n, m)
-        np.random.shuffle(locs)
+		# make the agents
+		num_agents = self.params.get('num_agents', 400)
 
-        # make the agents
-        num_agents = self.params.get('num_agents', 400)
-        self.agents = [Agent(locs[i], self.taxation, self.params)
-                       for i in range(num_agents)]
+		for i in range(num_agents):
+			self.add_agent()
 
-        # keep track of which cells are occupied
-        self.occupied = set(agent.loc for agent in self.agents)
+		# keep track of which cells are occupied
+		self.occupied = set(agent.loc for agent in self.agents)
 
-    def grow(self):
-        """Adds sugar to all cells and caps them by capacity."""
-        grow_rate = self.params.get('grow_rate', 1)
-        self.array = np.minimum(self.array + grow_rate, self.capacity)
+	def grow(self):
+		"""Adds sugar to all cells and caps them by capacity."""
+		grow_rate = self.params.get('grow_rate', 1)
+		self.array = np.minimum(self.array + grow_rate, self.capacity)
 
-    def look_around(self, center, vision):
-        """Finds the visible cell with the most sugar.
+	def look_around(self, center, vision):
+		"""Finds the visible cell with the most sugar.
 
-        center: tuple, coordinates of the center cell
-        vision: int, maximum visible distance
+		center: tuple, coordinates of the center cell
+		vision: int, maximum visible distance
 
-        returns: tuple, coordinates of best cell
-        """
-        # find all visible cells
-        locs = make_visible_locs(vision)
-        locs = (locs + center) % self.n
+		returns: tuple, coordinates of best cell
+		"""
+		# find all visible cells
+		locs = make_visible_locs(vision)
+		locs = (locs + center) % self.n
 
-        # convert rows of the array to tuples
-        locs = [tuple(loc) for loc in locs]
+		# convert rows of the array to tuples
+		locs = [tuple(loc) for loc in locs]
 
-        # select unoccupied cells
-        empty_locs = [loc for loc in locs if loc not in self.occupied]
+		# select unoccupied cells
+		empty_locs = [loc for loc in locs if loc not in self.occupied]
 
-        # if all visible cells are occupied, stay put
-        if len(empty_locs) == 0:
-            return center
+		# if all visible cells are occupied, stay put
+		if len(empty_locs) == 0:
+			return center
 
-        # look up the sugar level in each cell
-        t = [self.array[loc] for loc in empty_locs]
+		# look up the sugar level in each cell
+		t = [self.array[loc] for loc in empty_locs]
 
-        # find the best one and return it
-        # (in case of tie, argmax returns the first, which
-        # is the closest)
-        i = np.argmax(t)
-        return empty_locs[i]
+		# find the best one and return it
+		# (in case of tie, argmax returns the first, which
+		# is the closest)
+		i = np.argmax(t)
+		return empty_locs[i]
 
-    def harvest(self, loc):
-        """Removes and returns the sugar from `loc`.
+	def harvest(self, loc):
+		"""Removes and returns the sugar from `loc`.
 
-        loc: tuple coordinates
-        """
-        sugar = self.array[loc]
-        self.array[loc] = 0
-        return sugar
+		loc: tuple coordinates
+		"""
+		sugar = self.array[loc]
+		self.array[loc] = 0
+		return sugar
 
-    def step(self):
-        """Executes one time step."""
-        total_welfare = 0
-        replace = self.params.get('replace', False)
+	def step(self):
+		"""Executes one time step."""
+		total_welfare = 0
+		replace = self.params.get('replace', False)
 
-        taxes = [agent.get_tax() for agent in self.agents]
-        self.total_welfare = sum(taxes)
+	
+		# loop through the agents in random order
+		random_order = np.random.permutation(self.agents)
+		for agent in random_order:
 
-        # loop through the agents in random order
-        random_order = np.random.permutation(self.agents)
-        for agent in random_order:
+			# mark the current cell unoccupied
+			self.occupied.remove(agent.loc)
 
-            # mark the current cell unoccupied
-            self.occupied.remove(agent.loc)
+			# execute one step
+			agent.step_move_taxed(self)
 
-            # execute one step
-            agent.step(self)
+			# if the agent is dead, remove from the list
+			if agent.is_starving() or agent.is_old():
+				self.agents.remove(agent)
+				if replace:
+					self.add_agent()
+			else:
+				# otherwise mark its cell occupied
+				self.occupied.add(agent.loc)
+		taxes = [agent.get_tax() for agent in self.agents]
+		self.total_welfare = sum(taxes)
+		for agent in random_order:
+			agent.step(self)
 
-            # if the agent is dead, remove from the list
-            if agent.is_starving() or agent.is_old():
-                self.agents.remove(agent)
-                if replace:
-                    self.add_agent()
-            else:
-                # otherwise mark its cell occupied
-                self.occupied.add(agent.loc)
+		# update the time series
+		self.agent_count_seq.append(len(self.agents))
+		metabolisms = [agent.metabolism for agent in self.agents]
+		self.agent_metabolism_seq.append(np.mean(metabolisms))
 
-        # update the time series
-        self.agent_count_seq.append(len(self.agents))
-        metabolisms = [agent.metabolism for agent in self.agents]
-        self.agent_metabolism_seq.append(np.mean(metabolisms))
+		# grow back some sugar
+		self.grow()
+		return len(self.agents)
 
-        # grow back some sugar
-        self.grow()
-        return len(self.agents)
+	def get_welfare(self):
+		if len(self.agents) == 0:
+			return 0
+		return self.total_welfare / len(self.agents)
 
-    def get_welfare(self):
-        if len(self.agents) == 0:
-            return 0
-        return self.total_welfare / len(self.agents)
+	def add_agent(self):
+		"""Generates a new random agent.
 
-    def add_agent(self):
-        """Generates a new random agent.
+		returns: new Agent
+		"""
+		new_agent = Agent(self.random_loc(), self.taxation, self.params)
+		self.agents.append(new_agent)
+		self.occupied.add(new_agent.loc)
+		return new_agent
 
-        returns: new Agent
-        """
-        new_agent = Agent(self.random_loc(), self.params)
-        self.agents.append(new_agent)
-        self.occupied.add(new_agent.loc)
-        return new_agent
+	def random_loc(self):
+		"""Choose a random unoccupied cell.
 
-    def random_loc(self):
-        """Choose a random unoccupied cell.
+		returns: tuple coordinates
+		"""
+		while True:
+			loc = tuple(np.random.randint(self.n, size=2))
+			if loc not in self.occupied:
+				return loc
 
-        returns: tuple coordinates
-        """
-        while True:
-            loc = tuple(np.random.randint(self.n, size=2))
-            if loc not in self.occupied:
-                return loc
+	def plot_populations(self):
+		plt.plot(self.agent_count_seq)
 
-    def plot_populations(self):
-        plt.plot(self.agent_count_seq)
-
-    def plot_avg_metabolisms(self):
-        plt.plot(self.agent_metabolism_seq)
+	def plot_avg_metabolisms(self):
+		plt.plot(self.agent_metabolism_seq)
 
 
 
 if __name__ == '__main__':
-    env = Sugarscape(50, True, num_agents=400)
-    for i in range(800):
-        env.step()
-    env.plot_populations()
-    env = Sugarscape(50, False, num_agents=400)
-    for i in range(800):
-        env.step()
-    env.plot_populations()
-    plt.show()
+	x = 1
+	total_pops = []
+	for l in range(10):
+		env = Sugarscape(50, True, num_agents=400)
+		for i in range(800):
+			env.step()
+		total_pops.append(env.agent_count_seq)
+		print(l)
+		av_pops= np.mean(total_pops, axis=0)
+		plt.plot(av_pops)
+	#env.plot_populations()
+	
+	total_pops = []
+	for j in range(x):
+		env = Sugarscape(50, False, num_agents=400)
+		for i in range(800):
+			env.step()
+		total_pops.append(env.agent_count_seq)
+		print(j)
+	av_pops= np.mean(total_pops, axis=0)
+	plt.plot(av_pops)
+
+	#env.plot_populations()
+	plt.show()
